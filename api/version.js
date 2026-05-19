302bd90f5bd567fb106671358b8356e82a68f966
prellern1337 <m.emogstad@gmail.com>
2026-05-19 16:49:54 +0200

    Delete api/version.js

--- api/version.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(request, response) {
-  response.status(200).json({
-    package: "sb1-markets-nibor-ui-fix",
-    niborSource: "SpareBank 1 Markets Morgenrapport",
-    generatedAt: new Date().toISOString()
-  })
-}
